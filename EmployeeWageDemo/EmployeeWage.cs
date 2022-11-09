2c5d480ba9cdcc3addf5dfe373e539447c1bc76f
Vinay <navik.vinay12@gmail.com>
2022-11-09 12:35:54 +0530

    adding partTime feature in EmpWage UC3

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -7,17 +7,23 @@
             Console.WriteLine("Welcome to EmployeeWage Computation");
 
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
 
-            int empCheck=random.Next(0,2);
+            int empCheck=random.Next(0,3);
             if(empCheck== IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Full Time Employee is Present");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Part Time Employee is Present");
+                empHrs = 4;
             }
             else
             {
