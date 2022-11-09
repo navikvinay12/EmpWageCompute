2bb9f35b9f926a3f179178f1f6ceb6d5d2e288d7
Vinay <navik.vinay12@gmail.com>
2022-11-09 11:54:16 +0530

    Adding EmpPresent Or Absent UC1

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation");
+
+            int IS_FULL_TIME = 1;       //Constants
+            Random random = new Random();
+
+            int empCheck=random.Next(0,2);  //0 or 1
+            if(empCheck== IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");   //1-->Employee is Present
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");    //0-->Employee is Absent
+            }
             Console.ReadLine();
         }
     }
