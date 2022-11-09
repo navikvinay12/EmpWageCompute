1d5277de79225b97edd6bf8868ed720d46bbbfc2
Vinay <navik.vinay12@gmail.com>
2022-11-09 12:52:36 +0530

    adding EmpWageCalculation using Switch case UC4

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -1,34 +1,33 @@
-﻿namespace EmployeeWageDemo
+﻿using System;
+
+namespace EmployeeWageDemo
 {
     internal class EmployeeWage
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation");
-
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-
             int empCheck=random.Next(0,3);
-            if(empCheck== IS_FULL_TIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Full Time Employee is Present");
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("Part Time Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case IS_FULL_TIME :
+                    Console.WriteLine("Full Time Employee is Present");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME :
+                    Console.WriteLine("Part Time Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
             }
             empWage = EMP_RATE_PER_HOUR * empHrs;
             Console.WriteLine("Employee Wage : "+empWage);
