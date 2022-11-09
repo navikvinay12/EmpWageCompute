6df2863dfe73638f28bfff4d5ce751b727f0d452
Vinay <navik.vinay12@gmail.com>
2022-11-09 11:37:12 +0530

    EmpWage project-Adding welcome msg

---
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageDemo
+{
+    internal class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            Console.ReadLine();
+        }
+    }
+}
