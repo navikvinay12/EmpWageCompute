81160ea54164b6be6f1287785c2fbcf0e097e3b8
Vinay <navik.vinay12@gmail.com>
2022-11-10 11:10:16 +0530

    Calculated wages for a month

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -7,30 +7,37 @@ namespace EmployeeWageDemo
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation");
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck=random.Next(0,3);
-            switch (empCheck)
+            int TotalEmpWage = 0;
+            for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++)
             {
-                case IS_FULL_TIME :
-                    Console.WriteLine("Full Time Employee is Present");
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME :
-                    Console.WriteLine("Part Time Employee is Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                       // Console.WriteLine("Full Time Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                      //  Console.WriteLine("Part Time Employee is Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                     //   Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HOUR * empHrs;
+                TotalEmpWage += empWage;
             }
-            empWage = EMP_RATE_PER_HOUR * empHrs;
-            Console.WriteLine("Employee Wage : "+empWage);
+            Console.WriteLine("Assumed 20 Working Day in a Month the Employee Wage is : "+ TotalEmpWage);
             Console.ReadLine();
         }
     }
