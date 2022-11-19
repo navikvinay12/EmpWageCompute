2bacd3860e9e584936622107fbcd070cc9d09a7a
Vinay <navik.vinay12@gmail.com>
2022-11-19 13:00:22 +0530

    added constructor,ToString

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -1,46 +1,62 @@
-﻿using System;
-
-namespace EmployeeWageDemo
+﻿namespace TestingPractices
 {
-    internal class EmployeeWage
+    public class EmpWageBuilderObject
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) //20
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void ComputeEmpWage()
         {
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;      //variables
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)       //computation
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays) //computation .
             {
                 totalWorkingDays++;
-                Random random = new();
+                Random random = new Random();
                 int empCheck = random.Next(3);
                 switch (empCheck)
                 {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
                     case IS_FULL_TIME:
                         empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                totalEmpHrs += empHrs;//as the no of working days will be passing the total no of working hrs will going on added of each day accordingly .
-                if (totalEmpHrs > maxHoursPerMonth)  //totalEmpHrs should not go beyond defined MaxHrs .
-                {
-                    totalEmpHrs = maxHoursPerMonth;
-                }
-                //Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:#" + totalWorkingDays + " Emp Hrs" + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;//per hour rate * total working hour to find wage .
-            Console.WriteLine($"Total Emp Wage for the Company \"{company}\" is : {totalEmpWage}");
-            return totalEmpWage;
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + " is " + totalEmpWage);
         }
+        public override string ToString()
+        {
+            return "Total Emp Wage for company : " + company + " is " + totalEmpWage;
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            ComputeEmpWage("Dmart", 20, 3, 10);
-            ComputeEmpWage("Reliance", 25, 4, 20);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            dMart.ComputeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 20, 2, 10);
+            reliance.ComputeEmpWage();
+            Console.WriteLine(reliance.ToString());
         }
     }
 }
